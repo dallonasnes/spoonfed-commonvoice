df196cc63c6d0c94c78aeb439f15df00d01aeff6
Dallon Asnes <dallon.asnes@gmail.com>
2021-07-28 11:01:08 -0500

    Completed, now just logging

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from shutil import copyfile
 from typing import List
 import urllib.request
 import urllib.parse
+from collections import OrderedDict
+import tqdm
 
 """
 External setup:
@@ -83,11 +85,13 @@ def copy_files_to_anki_store(relative_audio_paths: List):
         first check if file exists in Anki store,
         if not then copy from local to there
     """
-    for audio_name in relative_audio_paths:
+    print("COPYING FILES TO ANKI MEDIA STORE")
+    for audio_name in tqdm(relative_audio_paths):
         dest_path = ANKI_MEDIA_PATH + audio_name
         if not os.path.isfile(dest_path):
             src_path = DIR_PATH + CLIPS_SUBPATH + audio_name
             copyfile(src_path, dest_path)
+    print("FINISHED COPYING FILES TO ANKI MEDIA STORE")
 
 def _get_word_frequency_map(sentences):
     vocab_freq_map = {}
@@ -99,12 +103,89 @@ def _get_word_frequency_map(sentences):
                 vocab_freq_map[word] = 1
     return vocab_freq_map
 
+def _invert_ordered_dict(map):
+    rtn_val = OrderedDict()
+    for key, value in map.items():
+        rtn_val[value] = key
+    return rtn_val
+
+def _remove_dup_sentences(ordered_map):
+    """Remove any sentences that don't introduce new words"""
+    deduped_ordered_map = OrderedDict()
+    previously_seen_words = set()
+    for key, sentence in ordered_map.items():
+        has_unseen_words = False
+        for word in sentence.split():
+            if word not in previously_seen_words:
+                has_unseen_words = True
+                previously_seen_words.add(word)
+        if has_unseen_words:
+            deduped_ordered_map[key] = sentence
+    return deduped_ordered_map
+
+def _order_sentences_by_min_num_new_words(sentences):
+    sentences_ordered_by_num_new_words = []
+    unused_sentences = sentences.copy()
+    previously_seen_words = set()
+    while len(unused_sentences) > 0:
+        print(len(unused_sentences))
+        sentence_new_word_count_map = {}
+        already_popped_sentence = False
+        for sentence in unused_sentences:
+            new_word_count = 0
+            just_seen_words = set()
+            for word in sentence.split():
+                if word not in previously_seen_words and word not in just_seen_words:
+                    new_word_count += 1
+                    just_seen_words.add(word)
+            if new_word_count < 2:
+                # we found a sentence with zero or one new word, so we can add it right away
+                idx = unused_sentences.index(sentence)
+                sentence = unused_sentences.pop(idx)
+                sentences_ordered_by_num_new_words.append(sentence)
+                for word in sentence.split():
+                    previously_seen_words.add(word)
+                already_popped_sentence = True
+                break #break out of for loop over unused_sentences
+            else:
+                # need to get new_word_count of all unused_sentences
+                sentence_new_word_count_map[sentence] = new_word_count
+        # now that we've checked all unused_sentences
+        # we can find the sentence with min new words and add to list
+        if not already_popped_sentence:
+            # first find sentence with min new word score
+            sentence, _ = min(sentence_new_word_count_map.items(), key=lambda x: x[1])
+            idx = unused_sentences.index(sentence)
+            sentence = unused_sentences.pop(idx)
+            sentences_ordered_by_num_new_words.append(sentence)
+            already_popped_sentence = True
+            for word in sentence.split():
+                previously_seen_words.add(word)
+    
+    return sentences_ordered_by_num_new_words        
+
+def _build_map_audio_to_sentence_ordered_by_min_new_words_in_sentence(sentences_ordered_by_min_num_new_words, sentence_to_audio_map):
+    rtn_val = OrderedDict()
+    for sentence in sentences_ordered_by_min_num_new_words:
+        audio_path = sentence_to_audio_map[sentence]
+        rtn_val[audio_path] = sentence
+    return rtn_val
+
+def _order_sentences_by_num_new_words(deduped_ordered_map):
+    sentence_to_audio_map = _invert_ordered_dict(deduped_ordered_map)
+    sentences_ordered_by_min_num_new_words = _order_sentences_by_min_num_new_words(list(sentence_to_audio_map.keys()))
+    map_audio_to_sentence_ordered_by_min_new_words_in_sentence = _build_map_audio_to_sentence_ordered_by_min_new_words_in_sentence(sentences_ordered_by_min_num_new_words, sentence_to_audio_map)
+    return map_audio_to_sentence_ordered_by_min_new_words_in_sentence
 
 def apply_ordering_to_notes(map):
     vocab_freq_map = _get_word_frequency_map(map.values())
-    # now want to score each sentence by audio path
-
-    return []
+    # order map by length of each sentence
+    ordered_map = OrderedDict(sorted(map.items(), key=lambda t: len(t[1].split())))
+    # reorder map by number of new words introduced in each sentence (compared to words previously seen)
+    map_ordered_by_num_new_words = _order_sentences_by_num_new_words(ordered_map)
+    # remove all sentences that intro 0 new words
+    deduped_ordered_audio_to_sentence_map = _remove_dup_sentences(map_ordered_by_num_new_words)
+    return deduped_ordered_audio_to_sentence_map
 
 def main():
     # read the csv into memory
