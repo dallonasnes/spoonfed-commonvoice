0f78013bcd6e670475aca88a48d472f1776d5735
Dallon Asnes <dallon.asnes@gmail.com>
2021-08-30 09:02:11 -0400

    Release 0.0.6 - Add option to filter to only complete sentences

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = (
 
 setuptools.setup(
     name="spoonfed_commonvoice",
-    version="0.0.5",
+    version="0.0.6",
     author="Dallon Asnes",
     author_email="dallon.asnes@gmail.com",
     description="CLI to generate Anki notes from Mozilla CommonVoice Datasets for language study",
